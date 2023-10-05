602b2c1ed1fb10275ad998563ec227d9886ae1fd
Yael246810 <yaelcohen691@gmail.com>
2023-10-05 17:16:49 +0300

    after solving the problem with the refresh

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -107,7 +107,8 @@ function Login() {
             <option value="CUSTOMER">CUSTOMER</option>
           </select>
         </div>
-        <button type="submit" disabled={!isValid || isSubmitted}>
+        <button type="submit">
+        {/* <button type="submit" disabled={!isValid || isSubmitted}> */}
           Login
         </button>
       </form>
@@ -116,3 +117,74 @@ function Login() {
 }
 
 export default Login;
+
+
+
+
+// This is Kobi's Login. the button disabled doesn't work here... I can "accidentally" send 10 requests...
+
+// import React from "react";
+// import { SubmitHandler, useForm } from "react-hook-form";
+//  import { zodResolver } from "@hookform/resolvers/zod";
+//  import * as zod from "zod";
+//  import "./login.css";
+//  import { LoginReqModel } from "../../Models/Login";
+//  import webApiService from "../../Services/CustomerWebApiService";
+//  import notifyService from "../../Services/NotificationService";
+//  import { useDispatch } from "react-redux";
+//  import { userLoggedInAction } from "../Redux/UserAppState";
+//  import { useNavigate } from "react-router-dom";
+//  import {
+//    loggedInAsAdmin,
+//    loggedInAsCompany,
+//    loggedInAsCustomer,
+//  } from "../Redux/GuardAppState";
+
+// function Login(): JSX.Element {
+
+//     const dispatch = useDispatch();
+
+//     const navigate = useNavigate();
+
+//     const schema = zod.object({
+//         email: zod.string().email("You should provide valid email"),
+//         password: zod.string().min(4, "Minimum 4 characters"),
+//     });
+
+//     const { register, handleSubmit,
+//         formState: { errors, isValid, isSubmitting } }
+//         = useForm<LoginReqModel>({ mode: "all", resolver: zodResolver(schema) });
+
+//     const onSubmit: SubmitHandler<LoginReqModel> = (data: LoginReqModel) => {
+
+//         return webApiService.login(data)
+//             .then(res => {
+//                 dispatch(userLoggedInAction(res.data));
+//                 navigate("/login")
+//                 if (data.email === 'admin@admin.com') {
+//                     dispatch(loggedInAsAdmin());
+//                     console.log("Logged in as admin...");
+//                 }
+
+//             })
+//             .catch(err => notifyService.error(err));
+
+//     };
+
+//     return (
+//         <div className="Login form-look-and-feel">
+//             <h1>Login</h1>
+//             <form onSubmit={(...args) => void handleSubmit(onSubmit)(...args)}>
+
+//                 {(errors?.email) ? <span>{errors.email.message}</span> : <label htmlFor="email">Email</label>}
+//                 <input {...register("email")} name="email" type="email" placeholder="Email..." />
+
+//                 {(errors?.password) ? <span>{errors.password.message}</span> : <label htmlFor="password">Password</label>}
+//                 <input {...register("password")} name="password" type="password" placeholder="Password..." />
+//                 <button disabled={!isValid || isSubmitting}>Login</button>
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default Login;
